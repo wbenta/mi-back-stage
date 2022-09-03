8037a0c6f5b0379317c85a63581152e596d1d059
吴本涛 <11313508+wu-bentao@user.noreply.gitee.com>
2022-09-03 14:15:49 +0800

    modified:   jsconfig.json 	modified:   package.json 	modified:   src/App.vue 	modified:   src/router/index.js 	deleted:    src/views/AboutView.vue 	deleted:    src/views/HomeView.vue 	new file:   src/views/index.vue 	new file:   src/views/login.vue

---
+++ src/views/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>这是首页的路由</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less"></style>
---
+++ src/views/login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>这是登录的路由</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less"></style>
